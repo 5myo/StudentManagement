c36cfa3fc4f1f8a18de452296552c6ce9bc68d89
5racoon <nafe0@naver.com>
2020-09-10 16:10:20 +0900

    feature : ui, uicontroller 추가
    ui 및 uicontroller 패키지를 생성한 후 디렉토리 안에 출력 시 콘솔 창에서 각 메뉴들을 볼 수 있도록 클래스를 추가하였다.

--- src/kr/bit/three/main/Program_Main.java
+++ src/kr/bit/three/main/Program_Main.java
@@ -1,10 +1,12 @@
 package kr.bit.three.main;
 
+import kr.bit.three.ui.StudentUi;
+import kr.bit.three.ui.MainUi;
+import kr.bit.three.uicontroller.MainController;
+
 public class Program_Main {
-
 	public static void main(String[] args) {
-		
-
+		MainController uic = new MainController();
+		uic.mainMethod();
 	}
-
 }
